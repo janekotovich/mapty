c230e302ebb9fa5131bba089287d5b682a13f012
Jane-Kotovich <jenyakotovich@gmail.com>
2022-01-15 12:46:53 +1000

    Geolocation API
    Setting up native geolocation API to get the coords

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 navigator.geolocation?.getCurrentPosition(
   function (position) {
-    console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
   },
   function () {
     alert('Could not get your position');
