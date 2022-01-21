088fbaed06706eafcbd53a4a7e89aef155b02dd9
Jane-Kotovich <jenyakotovich@gmail.com>
2022-01-21 14:57:11 +1000

    Connecting local storage for simple DB
    Rendering data on reload from storage

--- script.js
+++ script.js
@@ -64,7 +64,13 @@ class App {
   _workouts = [];
   _mapZoomLevel = 13;
   constructor() {
+    // get users position
     this._getPosition();
+
+    // get data from local storage
+    this._getLocalStorage();
+
+    // attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -241,6 +247,15 @@ class App {
   }
   _setLocalStorage() {
     localStorage.setItem('workouts', JSON.stringify(this._workouts));
+  }
+  _getLocalStorage() {
+    const data = localStorage.getItem('workouts');
+    const parsedData = JSON.parse(data);
+
+    if (!parsedData) return;
+
+    this._workouts = parsedData;
+    this._workouts.forEach(workout => this._renderWorkout(workout));
   }
 }
 
